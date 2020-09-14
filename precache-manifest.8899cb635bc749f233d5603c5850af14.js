4ae3f0654628f4861538b6e2271db1aef8dda77e
Karan Thakur <karanthakur715@gmail.com>
2020-09-14 13:25:15 +0530

    Updates

---
+++ precache-manifest.8899cb635bc749f233d5603c5850af14.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c02abf32b036a83a9ac49fa6bbd1e39",
+    "url": "/signuppage/index.html"
+  },
+  {
+    "revision": "c10b9e2b51474c091792",
+    "url": "/signuppage/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "483fff901c72ff7b9d62",
+    "url": "/signuppage/static/css/main.9c4a488a.chunk.css"
+  },
+  {
+    "revision": "c10b9e2b51474c091792",
+    "url": "/signuppage/static/js/2.5fb0208b.chunk.js"
+  },
+  {
+    "revision": "483fff901c72ff7b9d62",
+    "url": "/signuppage/static/js/main.b0bdfb1a.chunk.js"
+  },
+  {
+    "revision": "38f8cb2a78f403f06432",
+    "url": "/signuppage/static/js/runtime-main.24e1f699.js"
+  },
+  {
+    "revision": "0c0d8c2d73fef5ede69663aa13a2dec7",
+    "url": "/signuppage/static/media/background.0c0d8c2d.png"
+  }
+]);
